5312f4ea298018314dbedab062cc5f638069b0b5
lysanderuy <lysander.uy@gmail.com>
2026-07-13 04:06:07 +0800

    [Fix][Lysan] Fix Kotlin/KSP/JVM build config issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
@@ -7,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.lysanderuy.tulogs"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lysanderuy.tulogs"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -31,6 +32,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
